567e5edfd9467abe8aac31ce5365f69df18eb9cc
charles <2569337619@qq.com>
2023-09-27 11:13:18 +0800

    SemanticAnalysis init

--- semantic/SemanticAnalysis.java
+++ semantic/SemanticAnalysis.java
@@ -5,11 +5,22 @@ import syntax.AstNode;
 public class SemanticAnalysis {
     private AstNode rootAst;
 
+    private SemanticAnalysisChecker rootChecker;
+
     public SemanticAnalysis(AstNode rootAst) {
         this.rootAst = rootAst;
     }
 
     public void analysis() {
-        
+        SemanticAnalysisChecker rootChecker = new SemanticAnalysisChecker(rootAst);
+        this.rootChecker = rootChecker;
+        preTraverse(rootAst);
+    }
+
+    private void preTraverse(AstNode rootAst) {
+        for (AstNode astNode : rootAst.getChildList()) {
+            preTraverse(astNode);
+        }
+        rootChecker.checkAnalysis(rootAst);
     }
 }
